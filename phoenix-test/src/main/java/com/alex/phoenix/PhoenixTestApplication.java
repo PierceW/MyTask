d6229349730a7f4e66829da23abf1e587c5c9c62
AlexWong <wp_0925@hotmail.com>
2019-03-17 22:30:09 +0800

    update

---
+++ phoenix-test/src/main/java/com/alex/phoenix/PhoenixTestApplication.java
@@ -0,0 +1,12 @@
+package com.alex.phoenix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoenixTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoenixTestApplication.class);
+    }
+}
